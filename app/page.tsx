01ef807c13467644b6a91aac70ee970851f88adc
sujatagunale <sujata@jsmastery.pro>
2024-10-23 00:16:38 +0530

    004-setup_004-tailwindcss-setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <p className="text-3xl font-black text-violet-700">Welcome to Next.js 👋</p>
+    <p className="text-3xl font-black text-light-500">Welcome to Next.js 👋</p>
   );
 }
