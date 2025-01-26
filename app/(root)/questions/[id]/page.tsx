7f6909917db20c4a5e0939cb67a868b561022be0
sujatagunale <sujata@jsmastery.pro>
2025-01-26 23:19:35 +0530

    Question Details: Question Views (Better Approaches)

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 
 import TagCard from "@/components/cards/TagCard";
 import Preview from "@/components/editor/Preview";
 import { Metric } from "@/components/Metric";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
-import { getQuestion } from "@/lib/actions/question.action";
+import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
-
-import Views from "../view";
 
 const Page = async ({ params }: RouteParams) => {
   const { id } = await params;
   const { success, data: question } = await getQuestion({ questionId: id });
+
+  after(async () => {
+    await incrementViews({ questionId: id });
+  });
 
   if (!success || !question) return redirect("/404");
 
@@ -43,8 +46,6 @@ const Page = async ({ params }: RouteParams) => {
           {question.title}
         </h2>
       </div>
-
-      <Views questionId={id} />
 
       <div className="mb-8 mt-5 flex flex-wrap gap-4">
         <Metric
