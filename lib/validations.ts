b758adfe1f35eb28d3da0fc04c72804d1d120e24
sujatagunale <sujata@jsmastery.pro>
2025-02-12 16:08:17 +0530

    AI Integration: AI Answer API Route

--- lib/validations.ts
+++ lib/validations.ts
@@ -149,3 +149,17 @@ export const AnswerServerSchema = AnswerSchema.extend({
 export const GetAnswersSchema = PaginatedSearchParamsSchema.extend({
   questionId: z.string().min(1, "Question ID is required"),
 });
+
+export const AIAnswerSchema = z.object({
+  question: z
+    .string()
+    .min(5, {
+      message: "Question title must be at least 5 characters.",
+    })
+    .max(130, {
+      message: "Question title musn't be longer then 130 characters.",
+    }),
+  content: z.string().min(100, {
+    message: "Question description must have Minimum of 100 characters.",
+  }),
+});
