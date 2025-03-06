c1ebfb972bf5d9b0f43a5cb41d1e08c6a4d7e8d8
sujatagunale <sujata@jsmastery.pro>
2025-03-07 01:15:59 +0530

    Filters: Create Filter Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 import QuestionCard from "@/components/cards/QuestionCard";
 import DataRenderer from "@/components/DataRenderer";
+import CommonFilter from "@/components/filters/CommonFilter";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import ROUTES from "@/constants/routes";
 import { EMPTY_QUESTION } from "@/constants/states";
 import { getQuestions } from "@/lib/actions/question.action";
@@ -35,13 +37,19 @@ async function Home({ searchParams }: RouteParams) {
         </Button>
       </section>
 
-      <section className="mt-11">
+      <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch
           route={ROUTES.HOME}
           imgSrc="/icons/search.svg"
           placeholder="Search questions..."
           iconPosition="left"
           otherClasses="flex-1"
+        />
+
+        <CommonFilter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
         />
       </section>
 
--- constants/filters.ts
+++ constants/filters.ts
@@ -4,3 +4,30 @@ export const HomePageFilters = [
   { name: "Unanswered", value: "unanswered" },
   { name: "Recommended", value: "recommended" },
 ];
+
+export const AnswerFilters = [
+  { name: "Newest", value: "latest" },
+  { name: "Oldest", value: "oldest" },
+  { name: "Popular", value: "popular" },
+];
+
+export const CollectionFilters = [
+  { name: "Oldest", value: "oldest" },
+  { name: "Most Voted", value: "mostvoted" },
+  { name: "Most Viewed", value: "mostviewed" },
+  { name: "Most Recent", value: "mostrecent" },
+  { name: "Most Answered", value: "mostanswered" },
+];
+
+export const TagFilters = [
+  { name: "A-Z", value: "name" },
+  { name: "Recent", value: "recent" },
+  { name: "Oldest", value: "oldest" },
+  { name: "Popular", value: "popular" },
+];
+
+export const UserFilters = [
+  { name: "Newest", value: "newest" },
+  { name: "Oldest", value: "oldest" },
+  { name: "Popular", value: "popular" },
+];
